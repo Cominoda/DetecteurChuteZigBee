f07e0d1529945c5cd1016c8417051fb398dfe49f
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-01-29 14:39:29 +0100

    Ajout de la LED si chute + bouton

--- code/detection_de_chute.ino
+++ code/detection_de_chute.ino
@@ -5,14 +5,17 @@
 SoftwareSerial XBee(10, 11); // TX sur 10, RX sur 11
 MPU6050 mpu;
 
+const float CONVERSION = 16384.0;     // Diviseur pour obtenir l'accélération en g
 const float SEUIL_CHUTE_LIBRE = 0.3;  // Seuil en g pour la chute libre
-const float SEUIL_IMPACT = 2;       // Seuil en g pour l’impact
+const float SEUIL_IMPACT = 2;         // Seuil en g pour l’impact
 bool chuteDetectee = false;
 
 void setup() {
     Serial.begin(9600);
     Wire.begin();
     XBee.begin(9600);
+    pinMode(13, OUTPUT);
+    pinMode(4, INPUT);
 
     mpu.initialize();
     if (!mpu.testConnection()) {
@@ -24,11 +27,14 @@ void setup() {
 void loop() {
     int16_t AcX, AcY, AcZ;
     mpu.getAcceleration(&AcX, &AcY, &AcZ);
+    if (digitalRead(4) == LOW) { // Si le bouton est pressé
+      Serial.print("Ça fonctionne !");
+    } 
 
     // Convertir en "g"
-    float accelX = AcX / 16384.0;
-    float accelY = AcY / 16384.0;
-    float accelZ = AcZ / 16384.0;
+    float accelX = AcX / CONVERSION;
+    float accelY = AcY / CONVERSION;
+    float accelZ = AcZ / CONVERSION;
 
     // Calcul de l’accélération totale
     float totalAccel = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
@@ -51,6 +57,10 @@ void loop() {
         XBee.println("{\"ALERTE\": \"CHUTE\", \"X\": " + String(AcX) + ", \"Y\": " + String(AcY) + ", \"Z\": " + String(AcZ) + "}");
 
         chuteDetectee = true;
+
+        digitalWrite(13, HIGH); // On allume la LED
+        delay(3000); // On attend 3 secondes
+        digitalWrite(13, LOW); // On ré-éteint la LED
     }
       
     // Réinitialisation après 3 secondes pour détecter une nouvelle chute
